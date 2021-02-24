14ee58002d53f4bea4c2b79ae5d00c03b367417f
Najah Muhabbik <abiek2004@gmail.com>
2021-02-24 00:38:32 +0000

    UNubSarGang X2.8
    Signed-off-by: Najah Muhabbik <abiek2004@gmail.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export KERNELNAME=Moonlight
+export KERNELNAME=UNubSarGang
 
-export LOCALVERSION=Hikari-v1.9
+export LOCALVERSION=X2.8
 
 export KBUILD_BUILD_USER=frost
 
-export KBUILD_BUILD_HOST=otaku
+export KBUILD_BUILD_HOST=unubsar
 
 export TOOLCHAIN=clang
 
-export DEVICES=whyred,tulip,lavender,a26x
+export DEVICES=whyred
 
 export CI_ID=-1001463706805
 
@@ -20,9 +20,9 @@ source helper
 
 gen_toolchain
 
-send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred"
 
-send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred"
 
 START=$(date +"%s")
 
@@ -33,15 +33,15 @@ do
 	build ${i} -newcam
 done
 
-send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+send_msg "⏳ Start building Overclock version | DEVICES: whyred"
 
-send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+send_pesan "⏳ Start building Overclock version | DEVICES: whyred"
 
 git apply oc.patch
 
 for i in ${DEVICES//,/ }
 do
-	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	if [ $i == "whyred" ]
 	then
 		build ${i} -oldcam -overclock
 
@@ -53,6 +53,6 @@ END=$(date +"%s")
 
 DIFF=$(( END - START ))
 
-send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s, get nightly builds in @MoonlightCI | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
 
 send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
